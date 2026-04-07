4dedb1f7af6877bd77a4fd16b50a7b26c17d5b5c
sunkusminamisuita1 <sunkusminamisuita1@gmail.com>
2026-04-07 20:44:59 +0900

    ted

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ function getPDO(): PDO{
     if ($pdo === null) {
         $dsn = 'mysql:host=localhost;dbname=accounting;charset=utf8mb4';
         $user = 'test';
-        $Password = $_ENV['acctpwd'] ?? '';
+        $password = $_ENV['acctpwd'] ?? '';
         $pdo = new PDO($dsn, $user, $password, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
