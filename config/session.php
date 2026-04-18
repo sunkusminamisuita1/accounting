03386579e0f228b67260f6bcc764d2c4f525c6a3
sunkusminamisuita1 <sunkusminamisuita1@gmail.com>
2026-04-18 22:20:43 +0900

    dsfadf

--- config/session.php
+++ config/session.php
@@ -1,12 +1,14 @@
 <?php
 //require_once __DIR__.'/../config/bootstrap.php';
-echo "2.5".$_SESSION['ErrMsg']."<br>";
+$ErrMsg = $_SESSION['ErrMsg'] ?? "";
+echo "2.5" . $ErrMsg . "<br>";
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-echo "3".$_SESSION['ErrMsg']."<br>";
-if(isset($_SESSION['ErrMsg'])) {
+$ErrMsg = $_SESSION['ErrMsg'] ?? "";
+echo "3" . $ErrMsg . "<br>";
+if (!empty($ErrMsg)) {
     return DispErrorMsg();
 }
 return 0;
